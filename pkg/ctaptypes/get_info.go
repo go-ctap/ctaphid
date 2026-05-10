7c4d5095c528f3c41b04bc5eeb09d0f4ee76153e
Savely Krasovsky <savely@krasovs.ky>
2026-05-10 14:06:46 +0200

    refactor: add `omitempty` to JSON tags in `AuthenticatorGetInfoResponse` fields to omit empty values

--- pkg/ctaptypes/get_info.go
+++ pkg/ctaptypes/get_info.go
@@ -32,36 +32,36 @@ const (
 // AuthenticatorGetInfoResponse is used in Metadata Statement specification as well, so json notation added.
 type AuthenticatorGetInfoResponse struct {
 	Versions                         Versions                                      `cbor:"1,keyasint" json:"versions"`
-	Extensions                       []webauthntypes.ExtensionIdentifier           `cbor:"2,keyasint" json:"extensions"`
+	Extensions                       []webauthntypes.ExtensionIdentifier           `cbor:"2,keyasint" json:"extensions,omitempty"`
 	AAGUID                           uuid.UUID                                     `cbor:"3,keyasint" json:"aaguid"`
-	Options                          map[Option]bool                               `cbor:"4,keyasint" json:"options"`
-	MaxMsgSize                       *uint                                         `cbor:"5,keyasint" json:"maxMsgSize"`
-	PinUvAuthProtocols               []PinUvAuthProtocol                           `cbor:"6,keyasint" json:"pinUvAuthProtocols"`
-	MaxCredentialCountInList         *uint                                         `cbor:"7,keyasint" json:"maxCredentialCountInList"`
-	MaxCredentialIDLength            *uint                                         `cbor:"8,keyasint" json:"maxCredentialIdLength"`
-	Transports                       []string                                      `cbor:"9,keyasint" json:"transports"`
-	Algorithms                       []webauthntypes.PublicKeyCredentialParameters `cbor:"10,keyasint" json:"algorithms"`
-	MaxSerializedLargeBlobArray      *uint                                         `cbor:"11,keyasint" json:"maxSerializedLargeBlobArray"`
-	ForcePINChange                   *bool                                         `cbor:"12,keyasint" json:"forcePINChange"`
-	MinPINLength                     *uint                                         `cbor:"13,keyasint" json:"minPINLength"`
-	FirmwareVersion                  *uint                                         `cbor:"14,keyasint" json:"firmwareVersion"`
-	MaxCredBlobLength                *uint                                         `cbor:"15,keyasint" json:"maxCredBlobLength"`
-	MaxRPIDsForSetMinPINLength       *uint                                         `cbor:"16,keyasint" json:"maxRPIDsForSetMinPINLength"`
-	PreferredPlatformUvAttempts      *uint                                         `cbor:"17,keyasint" json:"preferredPlatformUvAttempts"`
-	UvModality                       *uint                                         `cbor:"18,keyasint" json:"uvModality"`
-	Certifications                   map[string]uint64                             `cbor:"19,keyasint" json:"certifications"`
-	RemainingDiscoverableCredentials *uint                                         `cbor:"20,keyasint" json:"remainingDiscoverableCredentials"`
-	VendorPrototypeConfigCommands    []uint                                        `cbor:"21,keyasint" json:"vendorPrototypeConfigCommands"`
-	AttestationFormats               []string                                      `cbor:"22,keyasint" json:"attestationFormats"`
-	UvCountSinceLastPinEntry         *uint                                         `cbor:"23,keyasint" json:"uvCountSinceLastPinEntry"`
-	LongTouchForReset                *bool                                         `cbor:"24,keyasint" json:"longTouchForReset"`
-	EncIdentifier                    *string                                       `cbor:"25,keyasint" json:"encIdentifier"`
-	TransportsForReset               []string                                      `cbor:"26,keyasint" json:"transportsForReset"`
-	PinComplexityPolicy              *bool                                         `cbor:"27,keyasint" json:"pinComplexityPolicy"`
-	PinComplexityPolicyURL           *string                                       `cbor:"28,keyasint" json:"pinComplexityPolicyURL"`
-	MaxPINLength                     *uint                                         `cbor:"29,keyasint" json:"maxPINLength"`
-	EncCredStoreState                *string                                       `cbor:"30,keyasint" json:"encCredStoreState"`
-	AuthenticatorConfigCommands      []uint                                        `cbor:"31,keyasint" json:"authenticatorConfigCommands"`
+	Options                          map[Option]bool                               `cbor:"4,keyasint" json:"options,omitempty"`
+	MaxMsgSize                       *uint                                         `cbor:"5,keyasint" json:"maxMsgSize,omitempty"`
+	PinUvAuthProtocols               []PinUvAuthProtocol                           `cbor:"6,keyasint" json:"pinUvAuthProtocols,omitempty"`
+	MaxCredentialCountInList         *uint                                         `cbor:"7,keyasint" json:"maxCredentialCountInList,omitempty"`
+	MaxCredentialIdLength            *uint                                         `cbor:"8,keyasint" json:"maxCredentialIdLength,omitempty"`
+	Transports                       []string                                      `cbor:"9,keyasint" json:"transports,omitempty"`
+	Algorithms                       []webauthntypes.PublicKeyCredentialParameters `cbor:"10,keyasint" json:"algorithms,omitempty"`
+	MaxSerializedLargeBlobArray      *uint                                         `cbor:"11,keyasint" json:"maxSerializedLargeBlobArray,omitempty"`
+	ForcePINChange                   *bool                                         `cbor:"12,keyasint" json:"forcePINChange,omitempty"`
+	MinPINLength                     *uint                                         `cbor:"13,keyasint" json:"minPINLength,omitempty"`
+	FirmwareVersion                  *uint                                         `cbor:"14,keyasint" json:"firmwareVersion,omitempty"`
+	MaxCredBlobLength                *uint                                         `cbor:"15,keyasint" json:"maxCredBlobLength,omitempty"`
+	MaxRPIDsForSetMinPINLength       *uint                                         `cbor:"16,keyasint" json:"maxRPIDsForSetMinPINLength,omitempty"`
+	PreferredPlatformUvAttempts      *uint                                         `cbor:"17,keyasint" json:"preferredPlatformUvAttempts,omitempty"`
+	UvModality                       *uint                                         `cbor:"18,keyasint" json:"uvModality,omitempty"`
+	Certifications                   map[string]uint64                             `cbor:"19,keyasint" json:"certifications,omitempty"`
+	RemainingDiscoverableCredentials *uint                                         `cbor:"20,keyasint" json:"remainingDiscoverableCredentials,omitempty"`
+	VendorPrototypeConfigCommands    []uint                                        `cbor:"21,keyasint" json:"vendorPrototypeConfigCommands,omitempty"`
+	AttestationFormats               []string                                      `cbor:"22,keyasint" json:"attestationFormats,omitempty"`
+	UvCountSinceLastPinEntry         *uint                                         `cbor:"23,keyasint" json:"uvCountSinceLastPinEntry,omitempty"`
+	LongTouchForReset                *bool                                         `cbor:"24,keyasint" json:"longTouchForReset,omitempty"`
+	EncIdentifier                    *string                                       `cbor:"25,keyasint" json:"encIdentifier,omitempty"`
+	TransportsForReset               []string                                      `cbor:"26,keyasint" json:"transportsForReset,omitempty"`
+	PinComplexityPolicy              *bool                                         `cbor:"27,keyasint" json:"pinComplexityPolicy,omitempty"`
+	PinComplexityPolicyURL           *string                                       `cbor:"28,keyasint" json:"pinComplexityPolicyURL,omitempty"`
+	MaxPINLength                     *uint                                         `cbor:"29,keyasint" json:"maxPINLength,omitempty"`
+	EncCredStoreState                *string                                       `cbor:"30,keyasint" json:"encCredStoreState,omitempty"`
+	AuthenticatorConfigCommands      []uint                                        `cbor:"31,keyasint" json:"authenticatorConfigCommands,omitempty"`
 }
 
 func (r *AuthenticatorGetInfoResponse) EffectiveMaxMsgSize() uint {
